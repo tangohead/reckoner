5ce73353bc46101afe0863034bb7464d91b14ece
matith <matthew.smith@cs.ox.ac.uk>
2021-01-23 21:08:11 +0000

    Adding TLS support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-gunicorn -w 4 -b 0.0.0.0:4000 reckoner.wsgi:app
+# check if cert exists, serve over HTTPS if so
+if [ -z "$CERT_PATH" ]
+then
+    gunicorn -w 4 -b 0.0.0.0:4000 reckoner.wsgi:app
+else
+    gunicorn -w 4 -b 0.0.0.0:4000 --certfile=$CERT_PATH --keyfile=$KEY_PATH reckoner.wsgi:app
+fi
